97f3b915190d07795b27f64c924727f5a0f3ff23
john davis <davis@doom.lan>
2024-06-09 11:30:14 -0400

    wip

---
+++ projects/s1.7_web_server4/server4.go
@@ -0,0 +1,41 @@
+// A web server
+// * returns lissajous gif
+//
+// Demos how returning a binary to the client via
+// another module can be done.
+//
+//
+// ch 1.7
+//
+// USAGE/DEMO:
+// Run this server and then use a web browser to view results
+//
+// Other notes
+//
+// all output mechanisms share a common interface via io.Writer
+//
+// * fetchall discards via ioutil.Discard
+// * webserver writes to http.ResponseWriter
+// * printf writes to os.Stdout
+
+package main
+
+import (
+	"log"
+	"net/http"
+	//"github.com/netskink/lissajous"
+)
+
+func main() {
+	// This is an anonymous function, he calls this
+	// a function literal
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		// This uses a module which needs to be installed
+		// cd ~..../projects/lissajous
+		// go build <-- previously done via ninja build
+		// go install <-- this installs the module in ~/go/bin/ workspace dir
+		lissajous(w)
+	}
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
